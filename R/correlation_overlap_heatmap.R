b650c688c0359692c0910f1e626f0261e57dd924
mding010705 <megan.ding@mail.utoronto.ca>
2025-10-31 01:09:37 -0400

    correlation overlap function corrected naming style

--- R/correlation_overlap_heatmap.R
+++ R/correlation_overlap_heatmap.R
@@ -8,16 +8,16 @@
 #' @param betas A dataframe with rows of genes and columns of tissue/cell types.
 #' Entry (i, j) is the TWAS effect size of gene i in tissue j, or NULL if gene i
 #' is not expressed/significant in tissue j.
-#' @param cor.method A string specifying the correlation method
+#' @param cor_method A string specifying the correlation method
 #' (pearson, kendall, spearman).
-#' @param tissue.names A vector of strings with length of the number of columns
+#' @param tissue_names A vector of strings with length of the number of columns
 #' in betas. These are the tissue names in the order that they appear in betas.
 #'
 #' @return Returns a heatmap plot of correlations and overlaps
 #'
 #' @examples
 #' correlation_overlap_heatmap(betas = data.frame(x = c(1, NA, 3), y = c(1, -9, 3),
-#'                             z = c(-1, 0, NA)), tissue.names = c("A", "B", "C"))
+#'                             z = c(-1, 0, NA)), tissue_names = c("A", "B", "C"))
 #'
 #' @export
 #' @import corrplot
@@ -25,21 +25,21 @@
 #' @import RColorBrewer
 
 
-correlation_overlap_heatmap <- function(betas, cor.method = "pearson",
-                                        tissue.names = 1:ncol(betas)){
+correlation_overlap_heatmap <- function(betas, cor_method = "pearson",
+                                        tissue_names = 1:ncol(betas)){
   if (ncol(betas) == 0){
     return(NULL)
   }
 
-  test_cor <- psych::corr.test(betas, method = cor.method, adjust = "fdr")
+  test_cor <- psych::corr.test(betas, method = cor_method, adjust = "fdr")
   corr_coef <- test_cor$r
-  colnames(corr_coef) <- tissue.names
-  row.names(corr_coef) <- tissue.names
+  colnames(corr_coef) <- tissue_names
+  row.names(corr_coef) <- tissue_names
   n_mat <- test_cor$n
-  colnames(n_mat) <- tissue.names
-  row.names(n_mat) <- tissue.names
-  colnames(n_mat) <- tissue.names
-  rownames(n_mat) <- tissue.names
+  colnames(n_mat) <- tissue_names
+  row.names(n_mat) <- tissue_names
+  colnames(n_mat) <- tissue_names
+  rownames(n_mat) <- tissue_names
   mat <- matrix(0, nrow = ncol(betas), ncol = ncol(betas))
   mat[upper.tri(mat, diag = TRUE)] <- test_cor$p.adj
   p_adj <- t(mat)
